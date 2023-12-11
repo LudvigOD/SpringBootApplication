878fc399926edcc92e343dcb41c61fe991583ae8
Mattias Nordahl <mattias.nordahl@cs.lth.se>
2023-12-11 16:46:19 +0100

    Some cleaning and Gradle improvements.

--- shared/src/test/java/utils/TestUtils.java
+++ shared/src/test/java/utils/TestUtils.java
@@ -1,5 +1,23 @@
 package utils;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import shared.Utils;
+
 public class TestUtils {
+
+    /*
+     * Just added some simple placeholder tests, for now. Gradle actually complains
+     * if no tests are run, so I added these to get rid of the warning.
+     */
+
+    @Test
+    public void testCreateUtil() { assertDoesNotThrow(() -> new Utils()); }
+
+    @Test
+    public void testHelper() { assertEquals("the helper", new Utils().helper()); }
 
 }
