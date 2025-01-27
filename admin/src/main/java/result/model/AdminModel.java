bac59cc115d47e4c450fe75dd316d59f459d6ad3
Jakob_Söderberg <ja4008so-s@student.lu.se>
2025-01-27 09:31:26 +0100

    Implemented Admin Model interface

--- admin/src/main/java/result/model/AdminModel.java
+++ admin/src/main/java/result/model/AdminModel.java
@@ -1,5 +1,46 @@
 package result.model;
 
-public class AdminModel {
-    
+import admin.view.RegisterView;
+
+public interface AdminModel {
+
+  void addListener(AdminView view);
+
+  void removeListener(AdminView view);
+
+  /**
+   * Registers a time for a start number using the current time.
+   * 
+   * @param startNbr
+   */
+  void registerTime(String startNbr);
+
+  /**
+   * Edits the time of a start number.
+   * 
+   * @param startNbr
+   */
+  void editTime(String startNbr);
+
+  /**
+   * Deletes a time for a start number.
+   * 
+   * @param startNbr
+   */
+  void deleteTime(String startNbr);
+
+  /**
+   * Change a start number.
+   * 
+   * @param startNbr
+   */
+  void editStartNbr(String startNbr);
+
+  /**
+   * Create a participant.
+   * 
+   * @param startNbr
+   */
+  void registerParticipant();
+
 }
