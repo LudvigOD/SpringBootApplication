8b0a8a1ba1c58c4849b5783ee4047839f4aa8ce0
Jesper Engberg <jeengbe@gmail.com>
2025-02-03 11:37:26 +0100

    fix: build failed

--- admin/src/main/java/result/AdminApplication.java
+++ admin/src/main/java/result/AdminApplication.java
@@ -1,5 +1,6 @@
 package result;
 
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -21,8 +22,8 @@ public class AdminApplication implements Runnable {
 
     public void run() {
         WebClient webClient = WebClient.builder()
-            .baseUrl("http://localhost:8080/api")
-            .build();
+                .baseUrl("http://localhost:8080/api")
+                .build();
         model = new AdminModelImpl(webClient);
         AdminGUI gui = new AdminGUI(model);
         gui.setVisible(true);
@@ -30,9 +31,8 @@ public class AdminApplication implements Runnable {
 
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduler.scheduleAtFixedRate(() -> {
-            model.getAllTimesFromServer(times -> model.updateTimeTable(times), 1, Optional.empty(), Optional.empty())
+            model.getAllTimesFromServer(times -> model.updateTimeTable(times), 1, Optional.empty(), Optional.empty());
         }, 0, 3, java.util.concurrent.TimeUnit.SECONDS);
-
 
     }
 
