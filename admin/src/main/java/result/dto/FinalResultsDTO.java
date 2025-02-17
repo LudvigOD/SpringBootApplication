abb0246e6f301fcf1456f681b7e219a89cf8aec5
viviantram1 <vivian1194.vt@gmail.com>
2025-02-17 09:01:31 +0100

    FinalResultDTOs

---
+++ admin/src/main/java/result/dto/FinalResultsDTO.java
@@ -0,0 +1,88 @@
+package result.dto;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.time.Duration;
+
+public class FinalResultsDTO {
+    private String teamToken;
+    private List<FinalResultDTO> jsonResult;
+
+    public FinalResultsDTO() {
+        // The automatic JSON conversion requires a default constructor
+    }
+
+    public FinalResultsDTO(String teamToken, List<FinalResultDTO> jsonResult) {
+        this.teamToken = teamToken;
+        this.jsonResult = jsonResult;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("FinalResultsDTO{teamToken='%s', jsonResult='%s'}", teamToken, jsonResult);
+    }
+    
+}
+
+class FinalResultDTO {
+    private String plac;
+    private String startNbr;
+    private String name;
+    private String totalTime;
+    private String startTime;
+    private String endTime;
+
+    public FinalResultDTO() {
+        // The automatic JSON conversion requires a default constructor
+    }
+
+    public FinalResultDTO(ResultDTO result, Integer startStation, Integer endStation) {
+        setPlace(result.getPlace());
+        this.startNbr = result.getCompetitor().getParticipant().getStartNbr();
+        this.name = result.getCompetitor().getParticipant().getName();
+        setTotalTime(result.getCompetitor().getTotalTime());
+        setStartTime(result.getCompetitor().getOnlyTimeForStation(startStation));
+        setEndTime(result.getCompetitor().getOnlyTimeForStation(endStation));
+    }
+
+    private void setPlace(Optional<Integer> place) {
+        if (place.isEmpty()){
+            this.plac = "DNF";
+        } else {
+            this.plac = place.get().toString();
+        }
+    }
+
+    private void setTotalTime(Optional<Duration> totalTime) {
+        if (totalTime.isEmpty()){
+            this.totalTime = "--:--:--";
+        } else {
+            this.totalTime = totalTime.get().toString();
+        }
+    }
+
+    private void setStartTime(Optional<Instant> startTime) {
+        if (startTime.isEmpty()){
+            this.startTime = "--:--:--";
+        } else {
+            this.startTime = startTime.get().toString();
+        }
+    }
+
+    private void setEndTime(Optional<Instant> endTime) {
+        if (endTime.isEmpty()){
+            this.endTime = "--:--:--";
+        } else {
+            this.endTime = endTime.get().toString();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("FinalResultDTO{plac='%s', startNbr ='%s', name ='%s', totalTime ='%s', startTime ='%s', endTime ='%s', }", plac, startNbr, name, totalTime, startTime, endTime);
+    }
+    
+}
+
+
